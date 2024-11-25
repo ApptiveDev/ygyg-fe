6ac5e483ec1779ce8183efb704c1cb27a7528265
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2024-11-25 11:24:48 +0900

    feature: 헤더 그림자

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -3,6 +3,8 @@ import SearchBar from '../SearchBar/SearchBar'
 import styles from './Header.module.scss'
 import logo from '@/assets/images/logo.svg'
 import Container from '@/components/atoms/Container/Container'
+import useHeaderScrollEffect from '@/hooks/useHeaderScrollEffect'
+import { css } from '@emotion/react'
 
 interface HeaderProps {
   isHome?: boolean
@@ -10,8 +12,10 @@ interface HeaderProps {
 
 export const Header = ({ isHome = false }: HeaderProps) => {
   const navigate = useNavigate()
+  const { headerStyle: scrollHeaderStyle } = useHeaderScrollEffect()
+
   return isHome ? (
-    <div className={styles.homeHeaderWrapper}>
+    <div className={styles.homeHeaderWrapper} css={scrollHeaderStyle}>
       <img src={logo} alt="logo" className={styles.homeLogo} onClick={() => navigate('/')} />
       <Container gap={24}>
         <div className={styles.textButton}>로그아웃</div>
@@ -19,7 +23,7 @@ export const Header = ({ isHome = false }: HeaderProps) => {
       </Container>
     </div>
   ) : (
-    <div className={styles.headerWrapper}>
+    <div className={styles.headerWrapper} css={scrollHeaderStyle}>
       <img src={logo} alt="logo" className={styles.logo} onClick={() => navigate('/')} />
       <SearchBar placeholder="검색하고 싶은 양념장을 입력하세요" />
       <div className={styles.textButton}>로그아웃</div>
