7a4515057e160a0fe8300fa2c4e2a8338d4b7015
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2025-01-06 14:31:30 +0900

    feat: 네이게이션 및 페이지별 플로팅버튼 유무

--- src/components/common/FloatingButton/FloatingButton.tsx
+++ src/components/common/FloatingButton/FloatingButton.tsx
@@ -1,9 +1,11 @@
 import styles from '@/components/common/FloatingButton/FloatingButton.module.scss'
 import FloatingImage from '@/assets/icons/floating-icon.svg'
+import { useNavigate } from 'react-router-dom'
 
 const FloatingButton: React.FC = () => {
+  const navigate = useNavigate()
   return (
-    <button className={styles.floatingBtn}>
+    <button className={styles.floatingBtn} onClick={() => navigate('/post')}>
       <img src={FloatingImage} alt="Floating Icon" className={styles.floatingIcon} />
       <span className={styles.floatingText}>
         새 양념장
