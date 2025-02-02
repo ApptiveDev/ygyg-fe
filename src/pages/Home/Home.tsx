04bb32b93e9e58b1c236fd3dbfffeda242fbb309
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2025-02-02 13:09:33 +0900

    feat: 홈화면 리스트 api 연결

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styles from './Home.module.scss'
 import SearchBar from '@/components/common/SearchBar/SearchBar'
@@ -7,12 +7,23 @@ import CardList from '@/components/common/CardList/CardList/CardList'
 import Container from '@/components/atoms/Container/Container'
 import Button from '@/components/common/Button/Button'
 import { GoArrowUpRight } from 'react-icons/go'
-import Banner from '@/components/features/Banner/Banner';
+import Banner from '@/components/features/Banner/Banner'
+import { CardData } from '@/api/hooks/card/types'
+import { getCategoryPostList } from '@/api/hooks/card/cardApi'
 
 export const HomePage = () => {
   const navigate = useNavigate()
   const [searchKeyword, setSearchKeyword] = useState('')
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
+  const [posts, setPosts] = useState<CardData[]>([])
+
+  const categoryMap: Record<string, number> = {
+    etc: 1,
+    liquid: 2,
+    sauce: 3,
+    powder: 4,
+    jam: 5,
+  }
 
   const categoryLabelMap: Record<string, string> = {
     liquid: '액체류',
@@ -20,7 +31,37 @@ export const HomePage = () => {
     powder: '가루류',
     jam: '잼류',
     etc: '기타',
-  };
+  }
+
+  const sortByMap: Record<string, string> = {
+    '최신 순': 'latest',
+    '약속 시간 임박 순': 'soonest',
+    '낮은 가격 순': 'lowestPrice',
+    '남은 인원 적은 순': 'lowestRemain',
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const categoryId = selectedCategory ? categoryMap[selectedCategory] : 0
+      const sortBy = 'latest'
+
+      try {
+        const response = await getCategoryPostList({
+          categoryId: categoryId ?? undefined,
+          sortBy,
+          page: 1,
+          size: 9,
+          isMinimumPeopleMet: false,
+        })
+
+        setPosts(response.items)
+      } catch (error) {
+        console.error('Error fetching posts:', error)
+      }
+    }
+
+    fetchPosts()
+  }, [selectedCategory])
 
   const search = () => {
     if (searchKeyword) navigate(`/list/search/${searchKeyword}`)
@@ -34,12 +75,13 @@ export const HomePage = () => {
     console.log(`선택된 카테고리: ${categoryName}`)
   }
 
-  const displayedCategory = selectedCategory ? categoryLabelMap[selectedCategory] || selectedCategory : '';
-
+  const displayedCategory = selectedCategory
+    ? categoryLabelMap[selectedCategory] || selectedCategory
+    : ''
 
   return (
     <Container size="full-width" direction="column" align="center">
-      <div className = {styles.bannerContainer}>
+      <div className={styles.bannerContainer}>
         <Banner />
       </div>
       <div className={styles.wrapper}>
@@ -68,15 +110,15 @@ export const HomePage = () => {
         <div className={styles.listContainer}>
           <span className={styles.commentMain}>
             {selectedCategory ? (
-                <>
-                  '<span className={styles.selectedCategoryText}>{displayedCategory}</span>' 소분 게시글
-                  목록
-                </>
-              ) : (
-                '양념장 소분 게시글 목록'
+              <>
+                '<span className={styles.selectedCategoryText}>{displayedCategory}</span>' 소분
+                게시글 목록
+              </>
+            ) : (
+              '양념장 소분 게시글 목록'
             )}
           </span>
-          <CardList selectedCategory={selectedCategory ?? undefined} />
+          <CardList cards={posts} selectedCategory={selectedCategory ?? undefined} />
 
           <Button
             className={styles.navigateButton}
